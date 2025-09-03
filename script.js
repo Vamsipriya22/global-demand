6269e9f1eda6b4143c5631480908ce7f597b746d
Goli Vamsi Priya <golivamsipriya22@gmail.com>
2025-09-03 16:47:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const map = new maplibregl.Map({
   style: "https://demotiles.maplibre.org/style.json", // Default MapLibre style
   center: [12, 44], // [Longitude, Latitude]
   zoom: 1.5,
-  minZoom: 1.25,
+  minZoom: 1.27,
   maxZoom: 6,
   pitch: 0,
   bearing: 0,
